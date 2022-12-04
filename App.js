fd3c1957b9383fb07bbc7271d8ec7c36734c5da2
Imdadul Haque <imdadulhaqueimdad820@gmail.com>
2022-12-04 23:52:50 +0600

    Folder Structure and working with database

---
+++ App.js
@@ -0,0 +1,16 @@
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello Native App</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
---
+++ components/Database/Database.js
@@ -0,0 +1,47 @@
+export const COLOURS = {
+  white: '#ffffff',
+  black: '#000000',
+  green: '#00AC76',
+  red: '#C04345',
+  blue: '#0043F9',
+  backgroundLight: '#F0F0F3',
+  backgroundMedium: '#B9B9B9',
+  backgroundDark: '#777777',
+};
+
+export const Items = [
+  {
+    id: 1,
+    category: 'product',
+    productName: 'MI Super Bass Bluetooth Wireless headphones',
+    productPrice: 1799,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex aliquam dolorem, doloremque ullam velit sint nulla quisquam adipisci enim accusantium dolore qui voluptate quia mollitia! Libero vel molestias hic maxime.',
+    isOff: true,
+    offPercentage: 10,
+    productImage: require('../Database/Images/products/Mi1.png'),
+    isAvailable: false,
+    productImageList: [
+      require('../Database/Images/products/Mi1.png'),
+      require('../Database/Images/products/Mi2.png'),
+      require('../Database/Images/products/Mi3.png'),
+    ],
+  },
+  {
+    id: 1,
+    category: 'product',
+    productName: 'MI Super Bass Bluetooth Wireless headphones',
+    productPrice: 1799,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex aliquam dolorem, doloremque ullam velit sint nulla quisquam adipisci enim accusantium dolore qui voluptate quia mollitia! Libero vel molestias hic maxime.',
+    isOff: true,
+    offPercentage: 10,
+    productImage: require('../Database/Images/products/Mi1.png'),
+    isAvailable: false,
+    productImageList: [
+      require('../Database/Images/products/Mi1.png'),
+      require('../Database/Images/products/Mi2.png'),
+      require('../Database/Images/products/Mi3.png'),
+    ],
+  },
+];
